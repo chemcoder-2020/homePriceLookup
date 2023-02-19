d53bc50632971c1ecbedd0f9ffe3d875d40e105b
chemcoder-2020 <59574987+chemcoder-2020@users.noreply.github.com>
2023-02-18 21:14:07 -0800

    Update homeLookup.py

--- homeLookup.py
+++ homeLookup.py
@@ -4,8 +4,10 @@ import pandas as pd
 st.set_page_config(layout="wide")
 
 homes = pd.read_excel("EstimatedValue.xlsx")
-homes.rename({"URL (SEE https://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)": "URL"}, inplace=True)
-homes.drop(columns=["FAVORITE","INTERESTED","LATITUDE","LONGITUDE"], inplace=True)
+
+homes["URL"] = homes["URL (SEE https://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)"]
+
+homes.drop(columns=["FAVORITE","INTERESTED","LATITUDE","LONGITUDE","URL (SEE https://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)"], inplace=True)
 st.write(homes)
 address = st.selectbox(label="Home Address", options=homes["ADDRESS"])
 query = homes[homes["ADDRESS"] == address]
