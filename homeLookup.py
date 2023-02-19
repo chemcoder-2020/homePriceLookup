53bba500d33193b5c2b247dc04debaf6fdeb0952
chemcoder-2020 <59574987+chemcoder-2020@users.noreply.github.com>
2023-02-18 21:58:55 -0800

    Update model and lookup

--- homeLookup.py
+++ homeLookup.py
@@ -1,17 +1,49 @@
 import streamlit as st
 import pandas as pd
+import pickle
+import numpy as np
+
+with open("homePricing.pkl", "rb") as f:
+    model = pickle.load(f)
 
 st.set_page_config(layout="wide")
 
-homes = pd.read_excel("EstimatedValue.xlsx")
+lookup, pretrained = st.tabs(["Lookup", "Pretrained Model"])
 
-homes["URL"] = homes["URL (SEE https://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)"]
+with lookup:
+    homes = pd.read_excel("EstimatedValue.xlsx")
 
-homes.drop(columns=["FAVORITE","INTERESTED","LATITUDE","LONGITUDE","URL (SEE https://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)"], inplace=True)
-st.write(homes)
-address = st.selectbox(label="Home Address", options=homes["ADDRESS"])
-query = homes[homes["ADDRESS"] == address]
-query["PriceToValue"] = query["PriceToValue"].round(2)
-for col in query.columns:
-    st.write(f"{col}: {query[col].values[0]}")
-# st.write(homes[homes["ADDRESS"] == address].T)
+    homes["URL"] = homes[
+        "URL (SEE https://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)"
+    ]
+
+    homes.drop(
+        columns=[
+            "FAVORITE",
+            "INTERESTED",
+            "LATITUDE",
+            "LONGITUDE",
+            "URL (SEE https://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)",
+        ],
+        inplace=True,
+    )
+    st.write(homes)
+    address = st.selectbox(label="Home Address", options=homes["ADDRESS"])
+    query = homes[homes["ADDRESS"] == address]
+    query["PriceToValue"] = query["PriceToValue"].round(2)
+    for col in query.columns:
+        st.write(f"{col}: {query[col].values[0]}")
+
+with pretrained:
+    cols = st.columns(len(model.feature_names_in_))
+    parameters = pd.DataFrame()
+    for i, col in enumerate(cols):
+        parameters[model.feature_names_in_[i]] = [
+            col.text_input(model.feature_names_in_[i])
+        ]
+    parameters.replace("", np.nan, inplace=True)
+    parameters["SQUARE FEET"] = parameters["SQUARE FEET"].astype(float)
+    parameters["$/SQUARE FEET"] = parameters["$/SQUARE FEET"].astype(float)
+    st.write(parameters)
+    if parameters.isna().sum().sum() == 0:
+        st.write(f"Estimated Price: ",model.predict(parameters)[0])
